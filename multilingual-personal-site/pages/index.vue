6d72a3b56695aa64575cf23fb67bb806fd77bf64
Lucas Medeiros <lucas.medeiros@dito.com.br>
2019-08-01 09:40:50 -0300

    Create nuxt app

---
+++ multilingual-personal-site/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-content">
+    <banner
+      banner-title="<strong>Lucas</strong> C. Medeiros"
+      banner-subtitle="Web developer - Frontend"
+    />
+    <v-layout column justify-center align-center>
+      <v-flex xs12 sm8 md6 />
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import Banner from '~/components/Banner.vue'
+
+export default {
+  components: {
+    Banner
+  }
+}
+</script>
+<style>
+.main-content {
+  position: relative;
+  left: 0;
+}
+</style>
